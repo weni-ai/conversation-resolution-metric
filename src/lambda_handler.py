bd3d1dc216adfc2811f89caef752f45d408c4229
Roger Alexandre Schwartzhaupt Nunes <103526740+RogerSchwartz@users.noreply.github.com>
2025-07-30 18:14:37 -0300

    Update lambda_handler.py

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -2,35 +2,71 @@ import json
 from openai import OpenAI
 import sys
 from get_parameter import get_parameter
+import os
+from pydantic import BaseModel
+from typing import Literal
 
+class ConversationResolutionResponse(BaseModel):
+    conversation_resolution: Literal["resolved", "unresolved"]
 
-
-def send_message(prompt):
+def send_message(prompt, model):
     OPENAI_API_KEY=get_parameter('OPENAI_API_KEY')
     client = OpenAI(api_key=OPENAI_API_KEY)
     response = client.chat.completions.create(
-        model="gpt-4o",
+        model=model,
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content.strip()
 
-def classify_text(text):
-    prompt = f'''Você é um especialista em analisar se uma conversa teve o problema do usuário resolvido ou não, ou se o usuário abandonou a conversa, ou seja você classifica entre "resolved" e "unresolved" ou "abandoned, faça isso olhando para a conversa toda e seguindo as intruções abaixo.
+def send_message_parse(prompt, model):
+    completion = client.chat.completions.parse(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "Você é um classificador especializado em analisar conversas."
+            },
+            {
+                "role": "user", 
+                "content": prompt
+            }
+        ],
+        # temperature=temperature,
+        # max_tokens=500,
+        response_format=ConversationResolutionResponse
+    )
+    
+    # Extrai o conteúdo da resposta
+    response = completion.choices[0].message.parsed
+    
+    # Converte o objeto Pydantic para dicionário
+    response_dict = response.model_dump() if hasattr(response, 'model_dump') else response.__dict__
+
+    return response_dict
+
+def classify_text(text, model):
+    prompt = f'''Você é um especialista em analisar se uma conversa teve o problema do usuário resolvido ou não, ou se o usuário abandonou a conversa, ou seja você classifica entre "resolved" e "unresolved", faça isso olhando para a conversa toda e seguindo as intruções abaixo.
 
     instruções:
-      - APENAS CLASSIFIQUE ENTRE "resolved" e "unresolved" ou "abandoned", NADA A MAIS QUE ISSO
-      - "abandoned" É QUANDO O USUÁRIO ABANDONA A CONVERSA.
+      - APENAS CLASSIFIQUE ENTRE "resolved" e "unresolved", NADA A MAIS QUE ISSO
+      - "unresolved" É QUANDO O chatbot confirma que não vai conseguir resolver a situação do cliente ou que o usuário NÃO CONTINUOU A CONVERSA e saiu SEM RESOLVER O PROBLEMA.
+      - "resolved" É QUANDO O CHATBOT RESOLVE TODOS OS  PROBLEMAS.
       - NÃO EXPLIQUE O PORQUE OU COMO FEZ A TAREFA APENAS FAÇA.
-      - SEU OUTPUT DEVE SER APENAS OU A TAG DE "resolvido" OU A TAG DE "não resolvido", NADA  MAIS.
+      - SEU OUTPUT DEVE SER APENAS OU A TAG DE "resolved" OU A TAG DE "unresolved", NADA  MAIS.
 
       Conversa completa: {text}
 
+
       OUTPUT:
     '''
-    return send_message(prompt)
+
+    print(f"Prompt: {prompt}")
+
+    return send_message_parse(prompt, model)
 
 def lambda_handler(event, context):
     input_text = event.get("conversation", "")
+    model = event.get("model", "gpt-4.1-nano")
     
     if not input_text:
         return {
@@ -41,7 +77,8 @@ def lambda_handler(event, context):
         }
 
     try:
-        classification = classify_text(input_text)
+        response = classify_text(input_text, model)
+        classification = response.get("conversation_resolution", "")
         return {
             'statusCode': 200,
             'body': {
